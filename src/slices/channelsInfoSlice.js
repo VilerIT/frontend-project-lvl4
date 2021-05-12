2717b27d630d01f1a632e1ab98a81ed7450fc62f
VilerIT <vilerinftech@gmail.com>
2021-05-12 13:40:29 +0400

    open new channel after creation

--- src/slices/channelsInfoSlice.js
+++ src/slices/channelsInfoSlice.js
@@ -18,7 +18,9 @@ export const channelsInfoSlice = createSlice({
       state.currentChannelId = id;
     },
     addChannel: (state, { payload: { channel } }) => {
+      const { id } = channel;
       state.channels.push(channel);
+      state.currentChannelId = id;
     },
     removeChannel: (state, { payload: { id } }) => {
       state.channels = state.channels.filter((channel) => (channel.id !== id));
